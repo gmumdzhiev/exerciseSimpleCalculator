059aed374b4cbf5d60d3f0e98cbd2dcceccb5d17
Georgi Mumdzhiev <george.mumdzhiev@gmail.com>
2018-04-26 15:52:02 +0200

    render

--- src/render.js
+++ src/render.js
@@ -13,10 +13,12 @@ function render(state) {
     button.addEventListener('click', function (event) {
       state.input += val
     })
+
     return button
   }
   function renderClearButton() {
-    const button = crel('button', 'clr')
+    const button = crel('button', 'C')
+    button.id = 'clear'
     button.addEventListener('click', function (event) {
       state.input = ''
     })
@@ -24,16 +26,18 @@ function render(state) {
   }
   function renderEvalButton() {
     const button = crel('button', '=')
+    button.id = 'result'
     button.addEventListener('click', function (event) {
       const result = eval(state.input)
       state.input = result
     })
     return button
   }
-  function renderButtons() {
-    const buttons = ['7', '8', '9', '6', '4', '5', '1', '2', '3', '0'].map(val => renderButton(val))
-    const operations = ['+', '-', '*', '/'].map(val => renderButton(val))
+  function renderButtons(numbers) {
+    const buttons = crel('div', { class: 'numb' }, ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0'].map(val => renderButton(val)))
+    const operations = crel('div', { class: 'opr' }, ['+', '-', '*', '/'].map(val => renderButton(val)))
     return crel('div', buttons, operations, renderClearButton(), renderEvalButton())
+
   }
   return crel('div',
     renderScreen(),
